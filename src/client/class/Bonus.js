94cfda757b29e6b5902639836c5932e90ccab047
Milan Theron <milan.theron.etu@univ-lille.fr>
2025-03-20 09:40:05 +0100

    carte affichage html + css

---
+++ src/client/class/Bonus.js
@@ -0,0 +1,14 @@
+const BonusType = {
+    EXTRA_LIFE: "Extra Life",
+    DOUBLE_POINTS: "Double Points",
+    SPEED_BOOST: "Speed Boost",
+    SHIELD: "Shield",
+    INVINCIBILITY: "Invincibility",
+    TIME_FREEZE: "Time Freeze"
+};
+
+class Bonus {
+    constructor(text) {
+        this.text = text;
+    }
+}
