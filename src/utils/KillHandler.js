df62f23c2b84feaffe5b1f25d32c6f9aefd65c42
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-03-23 11:59:13 +0100

    impl playerIsAlive

--- src/utils/KillHandler.js
+++ src/utils/KillHandler.js
@@ -2,7 +2,11 @@ import { START_SIZE } from '../client/class/Player.js';
 
 
 export class KillHandler{
+	_mapKill;
 
+	constructor() {
+		this._mapKill = new Map();
+	}
 
 	canKillPlayer(target, killer){
 		if (!target || !killer) {
@@ -17,6 +21,24 @@ export class KillHandler{
 		const distance = Math.hypot(target.x - killer.x, target.y - killer.y);
 		return distance < killer.size;
 	}
+	
+	killPlayer(target, killer){
+		if (this.canKillPlayer(target, killer)) {
+			this._mapKill.set(killer, [...(this._mapKill.get(killer) || []), target]);
+			return true;
+		}
+		return false;
+	}
+	
+	isPlayerAlive(player){
+		return !this._mapKill.values().some(killedPlayers => killedPlayers.includes(player));
+	}
+
+	getKills(player){
+		return this._mapKill.get(player) || [];
+	}
+	
+	
 
 
 }
